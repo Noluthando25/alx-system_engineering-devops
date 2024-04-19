c26189c62438338c06305d520f654676e1d4e243
Noluthando25 <nonhlanhlafumba@gmail.com>
2024-04-19 13:08:18 -0700

    Nolu

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 package { 'flask':
-  ensure   => '2.1.0'
-  provider => 'pip'
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -2,5 +2,5 @@
 exec { 'kill_killmenow_process':
   command => 'pkill killmenow',
   path    => ['/bin', '/usr/bin'],
-  onlyif  => "pgrep killmenow",
+  onlyif  => "pgrep killmenow"
 }
